268391567251b9a3d75c32a5b026737782c30d70
Ju Rodrigue <jurodrigues@Andy-Personal.local>
2023-08-22 14:10:28 -0300

    Estruturação inicial da section cards game

--- src/components/CardsGames/CardsGames.jsx
+++ src/components/CardsGames/CardsGames.jsx
@@ -1,10 +1,45 @@
 function CardsGames() {
-  return (  
+  return (
 
- <section>
+    <section className="s-games">
+      <div className="container">
+        <div className="left-top">
+          <span>GAMES</span>
+          <h2>Jogos exclusivos</h2>
+        </div>
+        <div className="rigt-top">
+          <ul className="js-nav-games">
+            <li>
+              <a href="">
+                <img src="assets/icon-game2.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="assets/icon-game1.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="assets/icon-game4.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="assets/icon-game3.svg" alt="" />
+              </a>
+            </li>
+          </ul>
 
-  
- </section>
+          <a href="" className="btn-all-games">
+            <img src="assets/icon-group-blue.svg" alt="" />
+            Ver todos os Jogos 
+          </a>
+        </div>
+      </div>
+
+
+    </section>
 
   );
 }
